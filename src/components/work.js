c016541e89e67f081d5efbbae7268ec788432d17
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-08-23 08:19:12 -0500

    refactoring the heading layout

--- src/components/work.js
+++ src/components/work.js
@@ -1,7 +1,5 @@
-import React, { Component } from "react"
+import React from "react"
 
-export default class Work extends Component {
-  render() {
-    return <div></div>
-  }
+export default function work() {
+  return <div></div>
 }
