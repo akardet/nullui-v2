02afe622d60a8db44948331bb49a98eb930517c3
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-09-02 15:18:06 -0500

    style social buttons for mobile

--- src/components/social.js
+++ src/components/social.js
@@ -10,29 +10,37 @@ export default function social() {
   return (
     <div className="social">
       <ul className="social__wrapper">
-        <li>
-          <a href="https://github.com/akardet">
+        <li className="icon github">
+          <a href="https://github.com/akardet" className="icon-github">
             <Github className="social__icon" />
+            Github
           </a>
         </li>
         <li>
-          <a href="https://codepen.io/nullui">
+          <a href="https://codepen.io/nullui" className="icon-codepen">
             <Codepen className="social__icon" />
+            Codepen
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/null_ui">
-            <Twitter className="social__icon" />
+          <a href="https://twitter.com/null_ui" className="icon-twitter">
+            <Twitter className="social__icon icon-twitter" />
+            Twitter
           </a>
         </li>
         <li>
-          <a href="https://instagram.com/nullui.co/">
-            <Instagram className="social__icon" />
+          <a href="https://instagram.com/nullui.co/" className="icon-instagram">
+            <Instagram className="social__icon icon--instagram" />
+            Instagram
           </a>
         </li>
         <li>
-          <a href="https://linkedin.com/in/sam-tancharoensuksavai/">
-            <Linkedin className="social__icon" />
+          <a
+            href="https://linkedin.com/in/sam-tancharoensuksavai/"
+            className="icon-linkedin"
+          >
+            <Linkedin className="social__icon icon--linkedin" />
+            LinkedIn
           </a>
         </li>
       </ul>
