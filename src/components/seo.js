138cf4e5380e5f98054c0b1cb4fc32272753324d
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-09-05 12:09:56 -0500

    change: path of twitter card image

--- src/components/seo.js
+++ src/components/seo.js
@@ -38,7 +38,6 @@ const SEO = ({ title, description, image, pathname, article }) => (
             {seo.description && (
               <meta property="og:description" content={seo.description} />
             )}
-            {seo.image && <meta property="og:image" content={seo.image} />}
             <meta name="twitter:card" content="summary_large_image" />
             {twitterUsername && (
               <meta name="twitter:creator" content={twitterUsername} />
@@ -47,7 +46,8 @@ const SEO = ({ title, description, image, pathname, article }) => (
             {seo.description && (
               <meta name="twitter:description" content={seo.description} />
             )}
-            {seo.image && <meta name="twitter:image" content={seo.image} />}
+            {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
+            {seo.image && <meta property="og:image" content={seo.image} />}
           </Helmet>
         </>
       )
